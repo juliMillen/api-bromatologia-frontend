a15184037bd9e19f57556cdda8c2886b1aba9b23
Julian Millen <julimillen@gmail.com>
2025-07-01 11:57:51 -0300

    agregando componentes a la plantilla HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmpresaComponent } from "./components/empresa/empresa/empresa.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegistroProductoComponent } from "./components/registro-producto/registro-producto.component";
+import { RegistroEstablecimientoComponent } from "./components/registro-establecimiento/registro-establecimiento.component";
+import { RegistroProductoEstablecimientoComponent } from "./components/registroProductoEstablecimiento/registro-producto-establecimiento/registro-producto-establecimiento.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmpresaComponent, LoginComponent, RegistroProductoComponent, RegistroEstablecimientoComponent, RegistroProductoEstablecimientoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
