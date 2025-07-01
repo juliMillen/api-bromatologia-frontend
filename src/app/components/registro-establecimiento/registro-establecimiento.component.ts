58d5e892e8c22067b389ed8c80b2581a73c3f4c0
Julian Millen <julimillen@gmail.com>
2025-07-01 11:52:10 -0300

    agregando componentes, services y entidades (models)

---
+++ src/app/components/registro-establecimiento/registro-establecimiento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-establecimiento',
+  imports: [],
+  templateUrl: './registro-establecimiento.component.html',
+  styleUrl: './registro-establecimiento.component.css'
+})
+export class RegistroEstablecimientoComponent {
+
+}
---
+++ src/app/components/registro-producto/registro-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-producto',
+  imports: [],
+  templateUrl: './registro-producto.component.html',
+  styleUrl: './registro-producto.component.css'
+})
+export class RegistroProductoComponent {
+
+}
