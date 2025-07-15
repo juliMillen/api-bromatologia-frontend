537af66c13400ed5dd1bdd46665f984541a6f97c
Julian Millen <julimillen@gmail.com>
2025-07-15 12:04:08 -0300

    agregando icono, actualizando pantalla de inico y rutas.

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavbarComponent,CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
