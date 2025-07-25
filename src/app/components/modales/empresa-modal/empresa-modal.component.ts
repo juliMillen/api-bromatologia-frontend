4d91e046346b2cf46ac563c1b3d7182ba9ee4024
Julian Millen <julimillen@gmail.com>
2025-07-25 20:33:46 -0300

    formulario reactivo para empresaModal

--- src/app/components/modales/empresa-modal/empresa-modal.component.ts
+++ src/app/components/modales/empresa-modal/empresa-modal.component.ts
@@ -1,12 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { Empresa } from '../../../models/empresa';
 import { EmpresaService } from '../../../services/empresa.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-empresa-modal',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './empresa-modal.component.html',
   styleUrl: './empresa-modal.component.css'
 })
@@ -34,13 +34,40 @@ export class EmpresaModalComponent implements OnInit {
   
     modoEdicion: boolean = false;
 
+    empresaForm!:FormGroup;
+
+    private fb = inject(FormBuilder);
+
 
     constructor(private empresaService: EmpresaService) {}
 
     ngOnInit(): void {
+
+      this.formularioEmpresa();
         if(this.empresaParaEditar){
-          this.empresa = JSON.parse(JSON.stringify(this.empresaParaEditar))
+          //this.empresa = JSON.parse(JSON.stringify(this.empresaParaEditar))
+          this.empresa = structuredClone(this.empresaParaEditar);
+          this.empresaForm.patchValue(this.empresa);
         }
+    }
+
+    formularioEmpresa(){
+
+      const crear = this.modo === 'crear';
+      const editar = this.modo === 'editar';
+
+      this.empresaForm = this.fb.group({
+        cuitEmpresa: [null,[Validators.required, Validators.pattern(/^\d{11}$/)]],
+        nombreEmpresa: ['', Validators.required],
+        email: ['', crear ? [Validators.required,Validators.email] : []],
+        telefono: ['', crear ? [Validators.required,Validators.pattern(/^(\+?\d{7,15})$/)] : []],
+        titular: this.fb.group({
+          cuitTitular: [null, [Validators.required,Validators.pattern(/^\d{11}$/)]],
+          nombreTitular: ['', Validators.required],
+          email: ['', crear ? [Validators.required, Validators.email] : []],
+          telefono: ['', crear ? [Validators.required,Validators.pattern(/^(\+?\d{7,15})$/)] : []]
+        })
+      });
     }
 
     guardarEmpresa():void {
@@ -52,25 +79,19 @@ export class EmpresaModalComponent implements OnInit {
     }
 
      crearEmpresa(): void {
-    this.empresaService.crearEmpresa(this.empresa).subscribe({
+      if(this.empresaForm.invalid){
+        this.empresaForm.markAllAsTouched();
+        return;
+      }
+
+      const nuevaEmpresa: Empresa = this.empresaForm.value;
+
+      this.empresaService.crearEmpresa(nuevaEmpresa).subscribe({
       next: (response: Empresa) => {
         console.log('Empresa creada correctamente: ', response);
         this.empresas.push(response);
-
-        //Reinicia el formulario
-        this.empresa = {
-          cuitEmpresa: 0,
-          nombreEmpresa: '',
-          email: '',
-          telefono: '',
-          titular: {
-            cuitTitular: 0,
-            nombreTitular: '',
-            email: '',
-            telefono: ''
-          }
-        };
-          this.cerrar.emit();
+        this.empresaForm.reset();
+        this.cerrar.emit();
       },
       error:(err) => {
         console.log('Error al crear la empresa: ',err);
